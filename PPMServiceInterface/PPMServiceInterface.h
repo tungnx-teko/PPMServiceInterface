2779c50b2f9d4a0f22937c17cf0be7363d6bdd6d
Tung Nguyen <xtungit@gmail.com>
2020-08-04 11:20:43 +0700

    Initial Commit

---
+++ PPMServiceInterface/PPMServiceInterface.h
@@ -0,0 +1,19 @@
+//
+//  PPMServiceInterface.h
+//  PPMServiceInterface
+//
+//  Created by Tung Nguyen on 8/4/20.
+//  Copyright © 2020 Tung Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PPMServiceInterface.
+FOUNDATION_EXPORT double PPMServiceInterfaceVersionNumber;
+
+//! Project version string for PPMServiceInterface.
+FOUNDATION_EXPORT const unsigned char PPMServiceInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPMServiceInterface/PublicHeader.h>
+
+
